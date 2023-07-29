2726906660a1290b3ebd4c3a600c4b3c18a1d9b7
ValentynVi <123833351+ValentynVi@users.noreply.github.com>
2023-07-29 11:32:53 +0100

    add js code

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+function createElements(arr) {
+  const markup = arr.map(item => {
+    // console.log(item);
+    const li = document.createElement('li');
+    li.textContent = `${item}`
+    li.classList.add('item')
+    console.log(li);
+    return li;
+  })
+  list.append(...markup);
+  console.log(list);
+
+}
+
+createElements(ingredients)
