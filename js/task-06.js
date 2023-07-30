2201699ef7fa6116a1f97fb7b0e5952962def6e7
ValentynVi <123833351+ValentynVi@users.noreply.github.com>
2023-07-30 16:35:34 +0100

    mod js code

--- js/task-06.js
+++ js/task-06.js
@@ -9,11 +9,10 @@ function handleBlur(evt) {
 
     if (evt.currentTarget.value.length === Number(input.dataset.length)) {
         evt.currentTarget.classList.add('valid');
+        evt.currentTarget.classList.remove('invalid');
     } else {
         evt.currentTarget.classList.remove('valid');
-
         evt.currentTarget.classList.add('invalid');
-
     }
 
 }
