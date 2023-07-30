82a92e186d4fb244be64a5db662a45fb4103ec57
ValentynVi <123833351+ValentynVi@users.noreply.github.com>
2023-07-30 19:40:41 +0100

    add js code

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,24 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const elements = {
+  btnChangeColor: document.querySelector('.change-color'),
+  spanAddText: document.querySelector('.color'),
+  body: document.querySelector('body'),
+}
+// const btnChangeColor = document.querySelector('.change-color');
+// const spanAddText = document.querySelector('.color');
+// const body = document.querySelector('body')
+
+elements.btnChangeColor.addEventListener('click', handleClick);
+
+function handleClick() {
+  elements.spanAddText.textContent = getRandomHexColor();
+  elements.body.style.backgroundColor = `${elements.spanAddText.textContent}`;
+
+}
+
+
+
+
