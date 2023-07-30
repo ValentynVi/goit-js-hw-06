128b7a373ec9163b5afa6348a8d78faa8e55a8be
ValentynVi <123833351+ValentynVi@users.noreply.github.com>
2023-07-30 18:34:13 +0100

    add js code

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form');
+console.dir(form);
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+    const { email, password } = evt.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        alert('Please fill in all the fields!');
+    } else {
+        const object = { email: email.value, password: password.value };
+        console.log(object);
+    }
+
+    form.reset();
+}
